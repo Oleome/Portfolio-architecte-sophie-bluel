f7c18eb729a0bdd240ff035e2612de7d75503096
Pierre <Pierre@DESKTOP-026O5CM.local>
2023-03-07 08:16:13 +0100

    fetch et boucle for operationnel

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,15 +1,19 @@
 const response = await fetch("http://localhost:5678/api/works");
 const works = await response.json();
-console.error(works);
 
-const project = works[0];
+
+for(let i=0; i<works.length; i++){
+
+const project = works[i];
+const sectionGallery = document.querySelector(".gallery");
 const figWorks = document.createElement("figure")
 const imageWorks = document.createElement("img");
 imageWorks.src = project.imageUrl;
 const captionWorks = document.createElement("figcaption");
 captionWorks.innerText = project.title;
 
-const sectionGallery = document.querySelector(".gallery");
+
 sectionGallery.appendChild(figWorks);
-sectionGallery.appendChild(imageWorks);
-sectionGallery.appendChild(captionWorks);
+figWorks.appendChild(imageWorks);
+figWorks.appendChild(captionWorks);
+}
