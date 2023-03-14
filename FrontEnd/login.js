f706206c212520857bf14e99eb8263455f72de77
Pierre Pillot <pierrepillot0110@gmail.com>
2023-03-14 16:50:15 +0100

    async

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,17 +1,16 @@
 function ajoutListenerIdentifiants() {
     const formulaireLogin = document.querySelector(".formulaire-login");
-    formulaireLogin.addEventListener("submit", function (event) {
-    event.preventDefault();
+    formulaireLogin.addEventListener("submit", async function (event) {
     const login = {
         email: event.target.querySelector("[name=email]").value,
         password: event.target.querySelector("[name=password]").value,
     };
     const chargeUtile = JSON.stringify(login);
-    const responseLogin = fetch("http://localhost:5678/api/users/login", {
+    const responseLogin = await fetch("http://localhost:5678/api/users/login", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: chargeUtile
-    });
+    }); console.error(responseLogin)
     responseLogin.then(response => {
         if (response.ok) {
             // Le formulaire est valide, on redirige vers la page d'accueil
