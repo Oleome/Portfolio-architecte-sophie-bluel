2a14e03ca283a7ff3d0ad2c570ad1f9aef3e4059
Pierre Pillot <pierrepillot0110@gmail.com>
2023-03-14 20:44:43 +0100

    fin de soirée

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -10,21 +10,8 @@ function ajoutListenerIdentifiants() {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: chargeUtile
-    }); console.error(responseLogin)
-    responseLogin.then(response => {
-        if (response.ok) {
-            // Le formulaire est valide, on redirige vers la page d'accueil
-            window.location.href = "index.html";
-        } else {
-            // Le formulaire est invalide, on affiche un message d'erreur
-            const messageErreur = document.createElement("div");
-            messageErreur.textContent = "Email ou mot de passe incorrect";
-            messageErreur.classList.add("message-erreur");
-            formulaireLogin.appendChild(messageErreur);
-        }
     });
     });
-
 } 
 
 ajoutListenerIdentifiants() 
