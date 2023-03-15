e21ad8c75d19b87c6c18d33754afe2082da015f5
Pierre Pillot <pierrepillot0110@gmail.com>
2023-03-15 13:00:22 +0100

    token stocké dans le local storage !!!!!

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -7,24 +7,25 @@ function ajoutListenerIdentifiants() {
             password: event.target.querySelector("[name=password]").value,
         };
         const chargeUtile = JSON.stringify(login);
-        const responseLogin = await fetch("http://localhost:5678/api/users/login", {
+        const request = await fetch("http://localhost:5678/api/users/login", {
             method: "POST",
             headers: {"Content-Type": "application/json"},
             body: chargeUtile
-        }) 
-        .then(response => {
-            if (response.ok) {
-                    // Le formulaire est valide, on redirige vers la page admin
-                    window.location.href = "./contact.html";
-                    localStorage.setItem("bonjour","les gens")
-            } else {
-                    // Le formulaire est invalide, on affiche un message d'erreur
-                    const messageErreur = document.createElement("div");
-                    messageErreur.textContent = "Email ou mot de passe incorrect";
-                    messageErreur.classList.add("message-erreur");
-                    formulaireLogin.appendChild(messageErreur);
-            }
-        });
+        }) .then(response => response)
+        
+        const data = await request.json()
+        if (request.ok) {
+            // Le formulaire est valide, on redirige vers la page admin
+            window.location.href = "./contact.html";
+            localStorage.setItem("token", data.token)
+        } else {
+            // Le formulaire est invalide, on affiche un message d'erreur
+            const messageErreur = document.createElement("div");
+            messageErreur.textContent = "Email ou mot de passe incorrect";
+            messageErreur.classList.add("message-erreur");
+            formulaireLogin.appendChild(messageErreur);
+        }
+        
     });
 } 
 
