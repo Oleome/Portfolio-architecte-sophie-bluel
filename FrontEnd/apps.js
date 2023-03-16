903f8e9bd435aeade611b7fbd8692bf01d83b87c
Pierre Pillot <pierrepillot0110@gmail.com>
2023-03-16 12:00:45 +0100

    fin de matinée

--- FrontEnd/apps.js
+++ FrontEnd/apps.js
@@ -7,10 +7,25 @@ const openModal = function (e) {
     target.removeAttribute('aria-hidden')
     target.setAttribute('aria-modal', 'true')
     modal = target
-    modal.addEventListener('click', closemodal)
+    modal.addEventListener('click', closeModal)
+    modal.querySelector('.js-modal-close').addEventListener('click', closeModal)
+    modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation)
 }
 
-const closeModal = 
+const closeModal = function (e) {
+    if(modal === null) return
+    e.preventDefault()
+    modal.style.display = "none"
+    modal.setAttribute('aria-hidden', 'true')
+    modal.removeAttribute('aria-modal')
+    modal.removeEventListener('click', closeModal)
+    modal.querySelector('.js-modal-close').removeEventListener('click', closeModal)
+    modal = null
+}
+
+const stopPropagation = function(e) {
+    e.stopPropagation
+}
 
 document.querySelectorAll('.js-modal').forEach( a => {
     a.addEventListener('click', openModal)
