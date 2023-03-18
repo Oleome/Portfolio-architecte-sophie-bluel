00cb273d70f139063472d1a39d03dae0966b6584
Bilou <pierrepillot0110@gmail.com>
2023-03-18 09:42:53 +0100

    admin bannière

--- FrontEnd/adminSession.js
+++ FrontEnd/adminSession.js
@@ -1,4 +1,7 @@
 if(localStorage.getItem('token')){
+    /**
+     * création bouton logout
+     */
     let log = document.querySelector('#login')
     log.innerText = 'logout'
     log.addEventListener('click', function(e) {
@@ -6,5 +9,13 @@ if(localStorage.getItem('token')){
         localStorage.removeItem('token')
         window.location.href = "./index.html";
     })
+
+
+    /**
+     * affichage bandeau admin
+     */
+    let bandeauAdmin = document.querySelector('.bandeau-admin') 
+    bandeauAdmin.style.display = null  
+    document.body.prepend(bandeauAdmin)
 }
 
